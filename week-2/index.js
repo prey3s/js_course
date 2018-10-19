5ba80d085cc1734bc789c673c0682daf9454046d
prey3s <paola.reyes@gmail.com>
2018-10-19 11:19:06 +0200

    Update index.js

--- week-2/index.js
+++ week-2/index.js
@@ -1,1 +1,45 @@
+/*
+Project: App for Finding Hairsalons with Available Appointments
+Classes:
+  Client
+  HairSalon 
+  Appointment
+  
+Interactions:
+  HairSalon offers appointments
+  Client can reserve appointment
+  
+*/
 
+
+var anna = new Client('Anna', 'short') 
+
+
+var HairSalon = class {
+  constructor(salonName) {
+    this.name = salonName
+    this.appointments = []
+  }
+  
+   offer(appointment) {
+    this.appointments.push(appointment)
+    appointment.available = true 
+   }
+}  
+  
+var HappyHair = new HairSalon("Happy Hair")
+
+var Appointment = class {
+  constructor(date, time, available) {
+    this.date = date
+    this.time = time
+    this.available = false
+    this.reservedBy = ""
+    }
+}
+
+var May1_noon = new Appointment ("May 1", "noon")  
+
+
+HappyHair.offer(May1_noon)
+anna.reserve(May1_noon)
