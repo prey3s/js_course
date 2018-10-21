841726690caa35ff26ab06d5f766627a64823a30
prey3s <paola.reyes@gmail.com>
2018-10-21 10:22:53 +0200

    Update index.js

--- week-2/index.js
+++ week-2/index.js
@@ -4,21 +4,34 @@ Classes:
   Client
   HairSalon 
   Appointment
-  
+  HairStylist
+
 Interactions:
   HairSalon offers appointments
-  Client can reserve appointment
-  
+  Client can books appointment
+  HairStylist works appointment
+
 */
 const Client = require('./client')
 const HairSalon = require('./hairsalon')
 const Appointment = require('./appointment')
+const HairStylist = require('./hairstylist')
 const Database = require('./database')
 
-var anna = new Client('Anna', 'short') 
-var HappyHair = new HairSalon("Happy Hair")
-var May1_noon = new Appointment ("May 1", "noon")  
+const Anna = new Client('Anna', 'short') 
+const HappyHair = new HairSalon("Happy Hair")
+const May1_noon = new Appointment ("May 1", "noon")  
+const Lula = new HairStylist ("Lula", 'color')
 
 
 HappyHair.offer(May1_noon)
-anna.reserve(May1_noon)
+Anna.books(May1_noon)
+Lula.works(May1_noon)
+
+
+Database.save(May1_noon)
+Database.save(Anna)
+Database.save(HappyHair)
+
+const loadedFile = Database.load()
+console.log(loadedFile.reservedBy)
