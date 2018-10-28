0271d1b097d7347b476fa988b5e295e131c6b99f
prey3s <paola.reyes@gmail.com>
2018-10-28 15:43:56 +0100

    Update index.js

--- week-2/index.js
+++ week-2/index.js
@@ -17,6 +17,7 @@ const HairSalon = require('./hairsalon')
 const Appointment = require('./appointment')
 const HairStylist = require('./hairstylist')
 const Database = require('./database')
+const fs = require('fs') //to include the file system module
 
 const Anna = new Client('Anna', 'short') 
 const Stacey = new Client('Stacey', 'long')
@@ -24,18 +25,36 @@ const HappyHair = new HairSalon("Happy Hair")
 const May1_noon = new Appointment ("May 1", "noon")  
 const Lula = new HairStylist ("Lula", 'color')
 const May3_2_00 = new Appointment ("May 3", "2:00") 
-const May3_1_00 = new Appointment ("May 3", "2:00") 
+const May3_1_00 = new Appointment ("May 3", "1:00") 
 
 HappyHair.offer(May1_noon)
 HappyHair.offer(May3_2_00)
 Anna.books(May1_noon)
 Lula.works(May1_noon)
 
-
-//Database.save(May1_noon)
-//Database.save(Anna)
 Database.save(HappyHair)
 
 const happyHairDatabase = Database.load()
 
-happyHairDatabase.offers(May3_1_00)
+console.log(happyHairDatabase.appointments[0])
+
+//Part II: async / await //
+
+const main = async () => {
+  const contents1 = await fs.readFile(_dirname + '/files/1.txt')
+  console.log(contents1)
+
+  const contents2 = await fs.readFile(_dirname + '/files/2.txt')
+  console.log(contents2)
+
+  const contents3 = await fs.readFile(_dirname + '/files/3.txt')
+  console.log(contents3)
+}
+
+(async () => {
+  try {
+      await main()
+  } catch (e) {
+      console.log(e)
+  }
+})()
