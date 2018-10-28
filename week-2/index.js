70d7f7a39225b19ae3ede1a24af19546c309a88b
prey3s <paola.reyes@gmail.com>
2018-10-28 13:22:37 +0100

    Update index.js

--- week-2/index.js
+++ week-2/index.js
@@ -19,19 +19,24 @@ const HairStylist = require('./hairstylist')
 const Database = require('./database')
 
 const Anna = new Client('Anna', 'short') 
+const Stacey = new Client('Stacey', 'long')
 const HappyHair = new HairSalon("Happy Hair")
 const May1_noon = new Appointment ("May 1", "noon")  
 const Lula = new HairStylist ("Lula", 'color')
-
+const May3_2_00 = new Appointment ("May 3", "2:00") 
 
 HappyHair.offer(May1_noon)
+HappyHair.offer(May3_2_00)
 Anna.books(May1_noon)
 Lula.works(May1_noon)
 
 
-Database.save(May1_noon)
-Database.save(Anna)
+//Database.save(May1_noon)
+//Database.save(Anna)
 Database.save(HappyHair)
 
-const loadedFile = Database.load()
-console.log(loadedFile.reservedBy)
+const HappyHairAppts = Database.load()
+
+const LulaNew = HappyHairAppts.appointments[0].stylist
+
+LulaNew.works(May3_2_00)
